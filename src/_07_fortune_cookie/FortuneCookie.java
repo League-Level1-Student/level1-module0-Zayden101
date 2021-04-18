68f99769b6009a46ed2ff476b4f62f054241255e
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2021-04-18 12:51:07 -0700

    e

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,9 +1,14 @@
 package _07_fortune_cookie;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class FortuneCookie {
+public class FortuneCookie implements ActionListener {
 	public void showButton() {
 		System.out.println("Button clicked");
 		JFrame frame = new JFrame();
@@ -13,6 +18,28 @@ public class FortuneCookie {
 		frame.add(button);
 		frame.pack();
 
+		button.addActionListener(this);
+}
 
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+	
+		int rand = new Random().nextInt(5);
+		if (rand ==(0)) {
+			JOptionPane.showMessageDialog(null, "a");
+		}
+			if (rand ==(1)) {
+				JOptionPane.showMessageDialog(null, "b");
+			}
+			if (rand ==(2)) {
+				JOptionPane.showMessageDialog(null, "c");
+			}
+			if (rand ==(3)) {
+				JOptionPane.showMessageDialog(null, "d");
+			}
+			if (rand ==(4)) {
+				JOptionPane.showMessageDialog(null, "e");
+			}
 	}
 }
--- src/_07_fortune_cookie/FortuneCookieRunner.java
+++ src/_07_fortune_cookie/FortuneCookieRunner.java
@@ -1,7 +1,8 @@
 package _07_fortune_cookie;
 
 public class FortuneCookieRunner {
-public static void main(String[] args) {
+	public static void main(String[] args) {
+		
 new FortuneCookie().showButton();
 }
 }
