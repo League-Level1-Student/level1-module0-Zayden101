8a2235d1be7055985ccfb270a606316d600adcfa
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2021-04-18 11:18:27 -0700

    j

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,5 +1,18 @@
 package _07_fortune_cookie;
 
-public class FortuneCookie {
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
+public class FortuneCookie {
+	public void showButton() {
+		System.out.println("Button clicked");
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		
+		JButton button = new JButton();
+		frame.add(button);
+		frame.pack();
+
+
+	}
 }
