63a199d5e87a3edc29a33ff6203792074f2b2433
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2021-05-16 13:02:50 -0700

    k

--- src/robort.java
+++ src/robort.java
@@ -12,37 +12,71 @@ public static void main(String[] args) {
 	a.miniaturize();
 	a.setSpeed(100);
 	a.penDown();
+	a.setPenWidth(7);
 	
 	Robot b=new Robot();
 	b.miniaturize();
 	b.setSpeed(100);
 	b.penDown();
+	b.setPenWidth(7);
 	
 	Robot c=new Robot();
 	c.miniaturize();
 	c.setSpeed(100);
 	c.penDown();
+	c.setPenWidth(7);
+	
+	Robot d=new Robot();
+	d.miniaturize();
+	d.setSpeed(100);
+	d.penDown();
+	d.setPenWidth(7);
+
+	Robot e=new Robot();
+	e.miniaturize();
+	e.setSpeed(100);
+	e.penDown();
+	e.setPenWidth(7);
+
+	Robot f=new Robot();
+	f.miniaturize();
+	f.setSpeed(100);
+	f.penDown();
+	f.setPenWidth(7);
 	
 	for (int i = 0; i < 250; i++) {
-		int x = new Random().nextInt(25); 
-		int y = new Random().nextInt(360); 
+	int x = new Random().nextInt(25); 
+	int y = new Random().nextInt(360); 
 	a.turn(y);
 	a.move(x);
-	//a.setRandomPenColor();
+	a.setRandomPenColor();
 	int z = new Random().nextInt(25); 
 	int w = new Random().nextInt(360); 
 	b.turn(w);
 	b.move(z);
-	//b.setRandomPenColor();
+	b.setRandomPenColor();
 	int v = new Random().nextInt(25); 
 	int u = new Random().nextInt(360); 
 	c.turn(u);
 	c.move(v);
-	//c.setRandomPenColor();
+	c.setRandomPenColor();
+	int s = new Random().nextInt(25); 
+	int t = new Random().nextInt(360); 
+	d.turn(t);
+	d.move(s);
+	d.setRandomPenColor();
+	int r = new Random().nextInt(25); 
+	int q = new Random().nextInt(360); 
+	e.turn(q);
+	e.move(r);
+	e.setRandomPenColor();
 	
-	
+	int o = new Random().nextInt(50); 
+	int p = new Random().nextInt(360); 
+	f.move(o);
+	f.turn(p);
+	f.setRandomPenColor();
 	}
-	
-	
+
 }
 }
---
+++ src/tif.java
@@ -0,0 +1,50 @@
+import java.awt.event.MouseEvent;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class tif {
+public static void main(String[] args) {
+	
+	Robot a=new Robot();
+	a.miniaturize();
+	a.setSpeed(999);
+	a.penDown();
+	
+	for (int i = 0; i < 35; i++) {
+		
+a.move(10);	
+a.turn(10);
+	}
+	for (int i = 0; i < 40; i++) {
+		
+a.move(10);	
+a.turn(9);
+	}
+	for (int i = 0; i < 46; i++) {
+		
+a.move(10);	
+a.turn(8);
+	}
+	for (int i = 0; i < 51; i++) {
+		
+a.move(10);	
+a.turn(7);
+	}
+	for (int i = 0; i < 61; i++) {
+		
+a.move(10);	
+a.turn(6);
+	}
+	for (int i = 0; i < 72; i++) {
+		
+a.move(10);	
+a.turn(5);
+	}
+	for (int i = 0; i < 89; i++) {
+		
+a.move(10);	
+a.turn(4);
+	}
+	
+}
+}
