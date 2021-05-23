d5453cf3a1a1275b5db7466a0eabce905c7b8091
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2021-05-23 12:49:33 -0700

    yt

--- src/tif.java
+++ src/tif.java
@@ -46,5 +46,116 @@ a.move(10);
 a.turn(4);
 	}
 	
+	
+	for (int i = 0; i < 35; i++) {
+		
+		a.move(10);	
+		a.turn(-10);
+			}
+			for (int i = 0; i < 40; i++) {
+				
+		a.move(10);	
+		a.turn(-9);
+			}
+			for (int i = 0; i < 46; i++) {
+				
+		a.move(10);	
+		a.turn(-8);
+			}
+			for (int i = 0; i < 51; i++) {
+				
+		a.move(10);	
+		a.turn(-7);
+			}
+			for (int i = 0; i < 61; i++) {
+				
+		a.move(10);	
+		a.turn(-6);
+			}
+			for (int i = 0; i < 72; i++) {
+				
+		a.move(10);	
+		a.turn(-5);
+			}
+			for (int i = 0; i < 89; i++) {
+				
+		a.move(10);	
+		a.turn(-4);
+			}
+			a.turn(90);
+			
+			for (int i = 0; i < 35; i++) {
+				
+				a.move(10);	
+				a.turn(10);
+					}
+					for (int i = 0; i < 40; i++) {
+						
+				a.move(10);	
+				a.turn(9);
+					}
+					for (int i = 0; i < 46; i++) {
+						
+				a.move(10);	
+				a.turn(8);
+					}
+					for (int i = 0; i < 51; i++) {
+						
+				a.move(10);	
+				a.turn(7);
+					}
+					for (int i = 0; i < 61; i++) {
+						
+				a.move(10);	
+				a.turn(6);
+					}
+					for (int i = 0; i < 72; i++) {
+						
+				a.move(10);	
+				a.turn(5);
+					}
+					for (int i = 0; i < 89; i++) {
+						
+				a.move(10);	
+				a.turn(4);
+					}
+					
+					for (int i = 0; i < 35; i++) {
+						
+						a.move(10);	
+						a.turn(-10);
+							}
+							for (int i = 0; i < 40; i++) {
+								
+						a.move(10);	
+						a.turn(-9);
+							}
+							for (int i = 0; i < 46; i++) {
+								
+						a.move(10);	
+						a.turn(-8);
+							}
+							for (int i = 0; i < 51; i++) {
+								
+						a.move(10);	
+						a.turn(-7);
+							}
+							for (int i = 0; i < 61; i++) {
+								
+						a.move(10);	
+						a.turn(-6);
+							}
+							for (int i = 0; i < 72; i++) {
+								
+						a.move(10);	
+						a.turn(-5);
+							}
+							for (int i = 0; i < 89; i++) {
+								
+						a.move(10);	
+						a.turn(-4);
+							}
+							a.move(10);
+					
 }
 }
