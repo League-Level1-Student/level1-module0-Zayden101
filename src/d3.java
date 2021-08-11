b10631fd860444f825133c878f61356facee89a7
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2021-08-11 16:53:31 -0700

    o

--- src/d3.java
+++ src/d3.java
@@ -11,6 +11,8 @@ public static void main(String[] args) {
 	a.penDown();
 	a.setPenWidth(3);
 
+	for (int i = 0; i < 13; i++) {
+
 	a.move(50);
 	a.turn(90);
 	a.move(50);
@@ -48,8 +50,11 @@ public static void main(String[] args) {
 	a.turn(90);
 	a.turn(45);
 	a.move(30);
-	
+	a.turn(10);
+	//a.setRandomPenColor();
 	a.penUp();
-	a.move(250);
+	a.move(100);
+	a.penDown();
+	}
 }
 }
