1e2c939865f1323e27036efdcdae401a76964a7c
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2021-09-12 12:50:56 -0700

    q

--- src/_10_cuteness_tv/ctty.java
+++ src/_10_cuteness_tv/ctty.java
@@ -4,39 +4,48 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class ctty {
-public static void main(String[] args) {
+public class ctty implements ActionListener {
+JButton button = new JButton();
+JButton button2 = new JButton();
+JButton button3 = new JButton();
+public void run() {
 	System.out.println("Button clicked");
 	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 	frame.setVisible(true);
 	
 
 	Dimension SMALL = new Dimension(200, 200);
 	
-	JButton button = new JButton();
-	frame.add(button);
+	
+	panel.add(button);
 	frame.pack();
 	button.setSize(SMALL);
 
-	JButton button2 = new JButton();
-	frame.add(button2);
+	
+	panel.add(button2);
 	frame.pack();
 	button2.setSize(SMALL);
 
-	JButton button3 = new JButton();
-	frame.add(button3);
+	
+	panel.add(button3);
 	frame.pack();
 	button3.setSize(SMALL);
+frame.add(panel);
 
 
 	
-	//button.addActionListener(this);
+	button.addActionListener(this);
+	button2.addActionListener(this);
+	button3.addActionListener(this);
 }
 
 	void showDucks() {
@@ -67,6 +76,22 @@ public static void main(String[] args) {
 	      }      } catch (Exception e) {
 	           e.printStackTrace();
 	      }
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		JButton buttonclick = (JButton) arg0.getSource();
+		if (buttonclick == button) {
+			showDucks();
+		}
+		if (buttonclick == button2) {
+			showFrog();
+		}
+		if (buttonclick == button3) {
+			showFluffyUnicorns();
+		}
 	}
 }
 
---
+++ src/_10_cuteness_tv/cttyrun.java
@@ -0,0 +1,9 @@
+package _10_cuteness_tv;
+
+public class cttyrun {
+public static void main(String[] args) {
+	
+	new ctty().run();
+}
+
+}
