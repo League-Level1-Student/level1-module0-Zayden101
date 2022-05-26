908b6ae6e6742ac0d521c5fe26cefcd5eac26452
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2022-05-26 11:25:59 -0700

    g

--- src/move.java
+++ src/move.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.util.Iterator;
 
 import javax.swing.JOptionPane;
@@ -9,17 +10,22 @@ public static void main(String[] args) {
 
 	int ang = 0;
 	int dis = 50;
+	int rep = 100;
+	String col = "Black";
+	
 	
 	Robot a = new Robot();
 	
 	a.penDown();
 	a.setSpeed(9999999);
+	a.setPenColor(Color.black);
+
 	
 	
 	for (int i = 0; i < 100; i++) {
 	
-	int move = JOptionPane.showOptionDialog(null, "Menu", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-			new String[] { "Forwards", "Backwards", "Left", "Right", "Turn", "Travel Distance", "Stop" }, null);
+	int move = JOptionPane.showOptionDialog(null, "Menu" + "          " + rep, "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Forwards", "Backwards", "Left", "Right", "Turn", "Travel Distance", "Color", "Miniaturize", "Stop" }, null); //ADD LINE THINKNESS, COLOR, (SIZE)
 	
 	//Forwards
 	if (move==0) {
@@ -27,9 +33,7 @@ public static void main(String[] args) {
 	}
 	//Backwards
 	if (move==1) {
-		//a.turn(180);
 		a.move(-dis);
-		//a.setAngle(ang);
 	}
 	//Left
 	if (move==2) {
@@ -128,14 +132,80 @@ public static void main(String[] args) {
 	}
 	}
 	
-	//Stop
+	
+	//color
 	if (move==6) {
+
+		for (int o = 0; i < 100; i++) {
+		int color = JOptionPane.showOptionDialog(null, "Select Travel Distance" + "               Color: " + col, "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Black", "White", "Red", "Orange", "Yellow", "Green", "Blue", "Back to Menu" }, null);
+		//black
+		if (color==0) {
+			a.setPenColor(Color.black);
+			col = "Black";
+		}
+		//white
+		if (color==1) {
+			a.setPenColor(Color.white);
+			col = "White";
+		}
+		//red
+		if (color==2) {
+			a.setPenColor(Color.red);
+			col = "Red";
+		}
+		//orange
+		if (color==3) {
+			a.setPenColor(Color.orange);
+			col = "Orange";
+		} 
+		//yellow
+		if (color==4) {
+			a.setPenColor(Color.yellow);
+			col = "Yellow";
+		}
+		//green
+		if (color==5) {
+			a.setPenColor(Color.green);
+			col = "Green";
+		} 
+		//blue
+		if (color==6) {
+			a.setPenColor(Color.blue);
+			col = "Blue";
+		} 
+		//Back to Menu
+		if (color==7) {
+			break;
+		} 
+	}
+	}
+		
+	
+	//Miniaturize
+	if (move==7) {
+	int mini = JOptionPane.showOptionDialog(null, "Are You Sure You Want To Miniaturize Your Robot? This Cannot Be Undone!", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Yes", "No" }, null);
+	//yes
+	if (mini==0) {
+		a.miniaturize();
+	}
+	//no
+	if (mini==1) {
+		a.move(0);
+	}
+	}
+	
+	
+	
+	//Stop
+	if (move==8) {
 		System.exit(0);
 	}
 	
 	
 	
-	
+	rep-=1;
 	
 	
 	
